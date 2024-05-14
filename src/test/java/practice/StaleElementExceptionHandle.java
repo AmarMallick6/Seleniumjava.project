5805b0d5eef7c38292c90cd14d48c0816dbda809
MAK <MAK@143MAK>
2024-05-14 17:45:08 +0530

    Commit :(3) update Selenium Version and Stale Element Excepsion Handle

--- src/test/java/practice/StaleElementExceptionHandle.java
+++ src/test/java/practice/StaleElementExceptionHandle.java
@@ -4,42 +4,46 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class StaleElementExceptionHandle
 
- {
+public class StaleElementExceptionHandle {
 
-	public static void main(String[] args)   {
-		WebDriver driver = new ChromeDriver();
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		driver.get("http:/primusbank.qedgetech.com/");
+		driver.get("http://primusbank.qedgetech.com/");
 		driver.findElement(By.linkText("Site Map")).click();
-		
-		List<WebElement> links = driver.findElements(By.tagName("a"));
-		System.out.println(links.size());
-		for(int i=0;i<links.size();i++)
-		{
-			String linkname = links.get(i).getText();
-			links.get(i).click();
-			String linkurl = driver.getCurrentUrl();
-			System.out.println(linkname+"    "+linkurl);
-			driver.navigate().back();
+		 List<WebElement>    Allinks=  driver.findElements(By.tagName("a"));
+		 int l=Allinks.size();
+		 System.out.println(l);
+		 for(int i=0;i<l;i++)
+		 {
+			 String lname=Allinks.get(i).getText();
+			 try {
+				 Allinks.get(i).click();
+				 String curl=driver.getCurrentUrl();
+				 System.out.println("LinkName : "+  lname+"        "+ "LinkUrl: "+ curl);
+				 driver.navigate().back();
+				// Thread.sleep(2000);
+				 Allinks=    driver.findElements(By.tagName("a"));
+			 }
+			 catch (StaleElementReferenceException e) {
+	                // Element is stale, refresh the list and continue
+	                Allinks = driver.findElements(By.tagName("a"));
+	                continue;
+	            }
+			 
 			
-			links = driver.findElements(By.tagName("a"));
-		}
-		driver.close();
+			 
+		 }
+		 driver.close();
+		
 
 	}
 
 }
-			
-			
-		
-			
-		
-
